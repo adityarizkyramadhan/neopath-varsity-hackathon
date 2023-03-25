77b89b6540634679697c39f3c46a258bddfb92d0
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-03-25 08:01:17 +0700

    update mentor.go

--- usecase/mentor.go
+++ usecase/mentor.go
@@ -1,6 +1,9 @@
 package usecase
 
-import "github.com/adityarizkyramadhan/neopath-varsity-hackathon/repositories"
+import (
+	"github.com/adityarizkyramadhan/neopath-varsity-hackathon/models"
+	"github.com/adityarizkyramadhan/neopath-varsity-hackathon/repositories"
+)
 
 type MentorUsecase struct {
 	repoGeneral *repositories.GeneralRepositoryImpl
@@ -10,3 +13,28 @@ func NewMentorUsecase(repoGeneral *repositories.GeneralRepositoryImpl) *MentorUs
 	return &MentorUsecase{repoGeneral: repoGeneral}
 }
 
+func (mu *MentorUsecase) GetAll() ([]*models.Mentor, error) {
+	var mentors []*models.Mentor
+
+	if err := mu.repoGeneral.FindAll(&mentors); err != nil {
+		return mentors, err
+	}
+
+	return mentors, nil
+}
+
+func (mu *MentorUsecase) GetById(id uint) (*models.Mentor, error) {
+	mentor := new(models.Mentor)
+	if err := mu.repoGeneral.FindById(id, mentor); err != nil {
+		return nil, err
+	}
+	return mentor, nil
+}
+
+func (mu *MentorUsecase) Register(arg models.MentorRegister) error {
+	mentor := new(models.Mentor)
+	mentor.Name = arg.Name
+	mentor.Occupation = arg.Name
+	mentor.Rating = arg.Rating
+	return mu.repoGeneral.Create(mentor)
+}
